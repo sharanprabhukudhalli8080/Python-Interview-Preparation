178b4139c49c87c6bc2029cb589a7c81abba0f0f
Sharanprabhu Kudhalli <sharanprabhukudhalli80@gmail.com>
2026-03-05 23:00:12 +0530

    Create break_continue_pass.py with control flow examples
    Add examples of break, continue, and pass statements in Python.

---
+++ 01_Python_Basics/02_Control_Flow/break_continue_pass.py
@@ -0,0 +1,21 @@
+# break_continue_pass.py
+
+# break example
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+
+# continue example
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+
+# pass example
+for i in range(5):
+    if i == 3:
+        pass
+    print(i)
