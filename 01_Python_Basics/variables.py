57483d0824db8a4063138d1272e4f4f698e5bbaf
Sharanprabhu Kudhalli <sharanprabhukudhalli80@gmail.com>
2026-03-09 02:15:10 +0530

    Create variables.py with variable examples
    Added a Python script demonstrating variable usage.

---
+++ 01_Python_Basics/variables.py
@@ -0,0 +1,20 @@
+# variables.py
+
+"""
+Interview Question:
+What is a variable in Python?
+
+Answer:
+A variable is used to store data in memory.
+Python is dynamically typed, so we don't need to declare data types.
+"""
+
+# Example
+
+age = 21
+name = "Sharan"
+price = 99.5
+
+print(age)
+print(name)
+print(price)
