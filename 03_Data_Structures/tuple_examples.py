a4a784394d8ce5faf5cea70077fd29fa3450686f
Sharanprabhu Kudhalli <sharanprabhukudhalli80@gmail.com>
2026-03-09 02:28:49 +0530

    Add tuple examples and explanation in tuple_examples.py
    Added an example of tuples in Python, including creation, access, and looping.

---
+++ 03_Data_Structures/tuple_examples.py
@@ -0,0 +1,20 @@
+"""
+Interview Question:
+What is a tuple in Python?
+
+Answer:
+A tuple is a collection of ordered elements.
+Tuples are immutable, meaning their values cannot be changed.
+"""
+
+# Creating a tuple
+numbers = (10, 20, 30)
+
+print("Tuple:", numbers)
+
+# Accessing element
+print("First element:", numbers[0])
+
+# Looping
+for i in numbers:
+    print(i)
