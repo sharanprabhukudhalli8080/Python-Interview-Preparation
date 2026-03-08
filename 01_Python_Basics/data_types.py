d0a165ff42a6f0e55098a2da30d632f0461a2135
Sharanprabhu Kudhalli <sharanprabhukudhalli80@gmail.com>
2026-03-09 02:16:38 +0530

    Add data_types.py with examples of Python data types

---
+++ 01_Python_Basics/data_types.py
@@ -0,0 +1,19 @@
+# data_types.py
+
+"""
+Interview Question:
+What are data types in Python?
+
+Answer:
+Data types define the type of value stored in a variable.
+"""
+
+a = 10
+b = 3.14
+c = "Python"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
