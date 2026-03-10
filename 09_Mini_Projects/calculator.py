7bbdce7c38c7c76db7a4b5f1cb4c66f314bc432d
Sharanprabhu Kudhalli <sharanprabhukudhalli80@gmail.com>
2026-03-10 22:49:09 +0530

    Create calculator.py

---
+++ 09_Mini_Projects/calculator.py
@@ -0,0 +1,28 @@
+# calculator.py
+
+print("Simple Calculator")
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+choice = int(input("Choose operation: "))
+
+if choice == 1:
+    print("Result:", a + b)
+
+elif choice == 2:
+    print("Result:", a - b)
+
+elif choice == 3:
+    print("Result:", a * b)
+
+elif choice == 4:
+    print("Result:", a / b)
+
+else:
+    print("Invalid choice")
